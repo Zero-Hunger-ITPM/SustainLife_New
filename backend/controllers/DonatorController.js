d1aad16d4e1606bc15e8980f5b555038e2fe6b2d
Sapooth Sanjaya <90241804+Sapooth@users.noreply.github.com>
2023-04-08 12:42:08 +0530

    Donator Backend Done

---
+++ backend/controllers/DonatorController.js
@@ -0,0 +1,104 @@
+const Donator = require("../models/DonatorModel");
+
+const addDonator = (req, res) => {
+  const {
+  first_name,
+  last_name,
+  address, 
+  phone_no,
+  donation_type,
+  d_qty, 
+  location
+     
+  } = req.body;
+
+  console.log(req.body);
+
+  const newDonator = new Donator({
+  first_name,
+  last_name,
+  address, 
+  phone_no,
+  donation_type,
+  d_qty, 
+  location
+  });
+
+  newDonator
+    .save()
+    .then((createdDonator) => {
+      res.status(200).json(createdDonator);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getDonator = async (req, res) => {
+  try {
+    const donator = await Donator.find();
+
+    res.status(200).json(donator);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const getsingleDonator = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const donator = await Donator.findById(id);
+    res.status(200).json(donator);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const updateDonator = async (req, res) => {
+  const donatorId = req.params.id;
+
+  try {
+    const donator = await Donator.findById(donatorId);
+
+    if(!donator){
+      return res.status(404).json("There is a no Donators");
+    }
+
+    const { first_name, last_name, address, phone_no, donation_type, d_qty, location} = req.body;
+    
+    const donato = await Donator.findByIdAndUpdate(donatorId, {first_name, last_name, address, phone_no, donation_type, d_qty, location});
+
+    res.status(201).json({
+      "Updated": true
+    })
+
+  } catch (error) { 
+    res.status(400).json(error.message);
+  }
+}
+
+const removeDonator = async (req, res) => {
+  const donatorId = req.params.id;
+
+  try {
+    const donator = await Donator.findById(donatorId);
+
+    if(!donator){
+      return res.status(404).json("There is a no Donator");
+    }
+
+    const removeDonator = await Donator.findByIdAndDelete(donatorId);
+    res.status(200).json(removeDonator);
+    
+  } catch (error) { 
+    res.status(400).json(error.message);
+  }
+}
+
+module.exports = {
+  addDonator,
+  getDonator,
+  getsingleDonator,
+  updateDonator,
+  removeDonator,
+};
